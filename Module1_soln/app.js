80670ee26a427aa46ca6624b3cb31118cd9d91f2
shivanshu1641 <55666330+shivanshu1641@users.noreply.github.com>
2020-06-25 21:15:06 +0530

    Update app.js

--- Module1_soln/app.js
+++ Module1_soln/app.js
@@ -5,12 +5,11 @@
 LunchCheckController.$inject = ['$scope'];
 	function LunchCheckController($scope){
 	$scope.itemlist = '';
-	$scope.list = $scope.itemlist.split(',').length ;
 	
 	$scope.sayMessage = function () {
 		$scope.list = $scope.itemlist.split(',').length ;
-    if($scope.list) {$scope.message = 'Enter Data first';} 
-	else if ($scope.list<=3) {$scope.message = 'Enjoy';} 
+    if($scope.list==1	) {$scope.message = 'Enter Data first';} 
+	else if($scope.list<=3 & $scope.list!=0) {$scope.message = 'Enjoy';} 
     else if($scope.list>=3) {$scope.message = 'Too Much !!!';}
   };
   $scope.printMessage = function () {
